38d3bcecfccd649670e8fc712dee9838795c3f99
AdieCode <adiecode4@gmail.com>
2025-02-26 23:09:49 +0200

    made some changes on how data get stored on the backend side

--- server/data/data.js
+++ server/data/data.js
@@ -101,18 +101,32 @@ const data = {
     },
 
     addVote: function(data, callback) {
-        const { sentence_id, vote } = data;
-    
-        let updateQuery;
-        if (vote === 'yes') {
-            updateQuery = 'UPDATE sentences SET yes_votes = yes_votes + 1 WHERE id = $1';
-        } else if (vote === 'no') {
-            updateQuery = 'UPDATE sentences SET no_votes = no_votes + 1 WHERE id = $1';
-        } else {
-            callback(new Error('Invalid vote type'), null);
-            return;
+        const { sentence_id, vote, selection } = data;
+
+        let column;
+        switch (selection) {
+            case 1:
+                column = vote === 'yes' ? 'yes_votes_1' : 'no_votes_1';
+                break;
+            case 2:
+                column = vote === 'yes' ? 'yes_votes_2' : 'no_votes_2';
+                break;
+            case 3:
+                column = vote === 'yes' ? 'yes_votes_3' : 'no_votes_3';
+                break;
+            case 4:
+                column = vote === 'yes' ? 'yes_votes_4' : 'no_votes_4';
+                break;
+            case 5:
+                column = vote === 'yes' ? 'yes_votes_5' : 'no_votes_5';
+                break;
+            default:
+                callback(new Error('Invalid selection value'), null);
+                return;
         }
-    
+
+        const updateQuery = `UPDATE sentences SET ${column} = ${column} + 1 WHERE id = $1`;
+
         codeSwitcher.query(updateQuery, [sentence_id], (err, res) => {
             if (err) {
                 console.error('Error updating votes:', err);
